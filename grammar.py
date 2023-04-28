3796bd748201af6c0f52a0ccc3e80d227292d973
Kristian Restrepo Osorio <103449864+kristianrpo@users.noreply.github.com>
2023-04-27 23:31:32 -0500

    Update "FIRST" function.
    - Corrections to the "FIRST" function.
    - Temporary documentation of the "FIRST" function.

--- grammar.py
+++ grammar.py
@@ -1,5 +1,4 @@
 class grammar():
-
     def __init__(self, nonterminals, terminals, productions, start):
         self.nonterminals = nonterminals
         self.terminals = terminals
@@ -22,26 +21,29 @@ def read_grammar():
 
 
 def FIRST(G, symbol, P, FIRST_SET):
-    for production in P:
-        counter = 0
-        for element in production:
-            if element in G.terminals:
-                FIRST_SET[symbol].append(element)
+    value = False # controlador para validar si una producción deriva en epsilon, así evitar eliminarlo del conjunto en caso de que pertenezca.
+    for production in P: # para cada produccion en la lista de producciones:
+        counter = 0 # permite saber si se recorrió toda la producción actual, para determinar que la misma deriva en epsilon.
+        for element in production: #para cada letra de la produccion:
+            if element in G.terminals: #si está en el conjunto de terminales, se agrega el terminal al first y se para de evaluar la produccion actual del no terminal.
+                FIRST_SET[symbol].add(element)
                 break
-            if element in G.nonterminals:
+            if element in G.nonterminals: # si está en el conjunto de no terminales, se ingresa al no terminal y se calcula el first al mismo (recursivamente).
                 counter += 1
-                if FIRST(G, element, G.productions[element], FIRST_SET):
-                    FIRST_SET[symbol].extend(FIRST_SET[element])
-                    FIRST_SET[symbol].remove('Ɛ')
-                else:
-                    FIRST_SET[symbol].extend(FIRST_SET[element])
+                if FIRST(G, element, G.productions[element], FIRST_SET): # si el llamado es true, significa que el no terminal tiene en su first epsilon y por lo tanto, se debe seguir evaluando la cadena.
+                    FIRST_SET[symbol] = FIRST_SET[symbol].union(FIRST_SET[element])
+                else: # si el llamado es false, significa que el no terminal no tiene epsilon en su first, y por lo tanto, no se debe seguir evaluando la cadena.
+                    FIRST_SET[symbol] = FIRST_SET[symbol].union(FIRST_SET[element])
                     break
-
-                if counter == len(production):
-                    if 'Ɛ' in FIRST_SET[element]:
-                        FIRST_SET[symbol].append('Ɛ')
-
+                if 'Ɛ' in FIRST_SET[symbol] and value == False: # se elimina el epsilon del first de el no terminal actual, ya que el mismo solo deriva en epsilon cuando en sus producciones tiene epsilon o se recorrio toda la produccion y cada letra deriva en epsilon (se evalua después).
+                    FIRST_SET[symbol].remove('Ɛ')
+                if counter == len(production): # si el contador es igual al largo de la produccion, significa que se recorrio toda la cadena.
+                    if 'Ɛ' in FIRST_SET[element]: # si en el first de mi ultimo elemento está epsilon, mi cadena puede derivar en epsilon, ya que si recorrimos toda la cadena es porque cada elemento de la misma deriva en epsilon.
+                        FIRST_SET[symbol].add('Ɛ')
+                        value = True # se cambia el valor del controlador para no eliminar el epsilon agregado en caso de que existan más producciones con el simbolo no terminal actual.
     if 'Ɛ' in P:
+        FIRST_SET[symbol].add('Ɛ')
+    if 'Ɛ'in FIRST_SET[symbol]: ## condición del llamado recursivo, con lo que se determina si el anterior llamado recursivo debe continuar evaluando la cadena o no.
         return True
     else:
         return False
@@ -54,12 +56,10 @@ def main():
     G = grammar(nonterminals, terminals, productions, start)
     FIRST_SET = {}
     for i in nonterminals:
-        FIRST_SET[i] = []
-
+        FIRST_SET[i] = set()
+    for i in FIRST_SET.keys():
+        FIRST(G, i, productions[i], FIRST_SET)
     print(FIRST_SET)
-    FIRST(G, start, productions[start], FIRST_SET)
-    print(FIRST_SET)
-
 
 if __name__ == "__main__":
     main()
